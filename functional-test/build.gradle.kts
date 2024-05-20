9ccf3e3100d468a03779981e82f205d45ec926ee
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-05-20 22:39:36 +0900

    apply grpc

--- functional-test/build.gradle.kts
+++ functional-test/build.gradle.kts
@@ -11,7 +11,8 @@ description = "Function test module for protoc-gen-kotlin-ext"
 
 dependencies {
     implementation(libs.protobuf.java)
-    implementation("com.google.protobuf:protobuf-kotlin:3.25.3")
+    implementation(libs.grpc.java.protobuf)
+    implementation(libs.grpc.kotlin.stub)
 }
 
 protobuf {
@@ -23,6 +24,12 @@ protobuf {
             path = project(":protoc-gen-kotlin-ext").layout.buildDirectory.asFile.get()
                 .resolve("install/protoc-gen-kotlin-ext/bin/protoc-gen-kotlin-ext").path
         }
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${libs.versions.grpc.java.get()}"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:${libs.versions.grpc.kotlin.get()}:jdk8@jar"
+        }
     }
     generateProtoTasks {
         all().forEach { task ->
@@ -32,6 +39,14 @@ protobuf {
                 id("kotlin-ext") {
                     outputSubDir = "kotlin"
                     option("messageOrNullGetter+")
+                }
+                // Test to ensure there are no issues when using grpc-java.
+                id("grpc") {
+                    outputSubDir = "java"
+                }
+                // Test to ensure there are no issues when using grpc-kotlin.
+                id("grpckt") {
+                    outputSubDir = "kotlin"
                 }
             }
         }
