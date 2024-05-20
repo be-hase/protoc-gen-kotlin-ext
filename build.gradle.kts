cab82b2f421136011c8fe8434742d4b0edf81d5b
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-05-21 01:38:23 +0900

    incr version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@ val isOnCI = System.getenv()["GITHUB_ACTIONS"] != null
 
 allprojects {
     group = "dev.hsbrysk"
-    version = "0.0.1" + if (isOnCI) "" else "-SNAPSHOT"
+    version = "0.0.2" + if (isOnCI) "" else "-SNAPSHOT"
 }
