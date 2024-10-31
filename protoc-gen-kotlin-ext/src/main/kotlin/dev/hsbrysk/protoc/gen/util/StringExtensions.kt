604ae110428eb48a2a6aa2bc5781926437c8b934
오지환 (Cooper) <cooper@querypie.com>
2024-10-31 09:24:59 +0900

    fix: Fix name convertor (#50)

--- protoc-gen-kotlin-ext/src/main/kotlin/dev/hsbrysk/protoc/gen/util/StringExtensions.kt
+++ protoc-gen-kotlin-ext/src/main/kotlin/dev/hsbrysk/protoc/gen/util/StringExtensions.kt
@@ -3,17 +3,49 @@ package dev.hsbrysk.protoc.gen.util
 /**
  * Convert kebab case or snake case to Pascal case (a.k.a. upper camel case).
  */
-internal fun String.pascalCase(): String = hyphenToUnderscore().split("_").joinToString("") { value ->
-    value.replaceFirstChar { it.uppercase() }
+internal fun String.pascalCase(): String = buildString {
+    var capitalizeNext = true
+    for (c in hyphenToUnderscore()) {
+        when {
+            c == '_' -> {
+                capitalizeNext = true
+            }
+            c.isDigit() -> {
+                append(c)
+                capitalizeNext = true
+            }
+            else -> {
+                append(if (capitalizeNext) c.uppercase() else c)
+                capitalizeNext = false
+            }
+        }
+    }
 }
 
 /**
  * Convert kebab case or snake case to camel case.
  */
-internal fun String.camelCase(): String =
-    hyphenToUnderscore().split("_").withIndex().joinToString("") { (index, value) ->
-        if (index == 0) value.replaceFirstChar { it.lowercase() } else value.replaceFirstChar { it.uppercase() }
+internal fun String.camelCase(): String = buildString {
+    var capitalizeNext = false
+    for ((i, c) in hyphenToUnderscore().withIndex()) {
+        when {
+            c == '_' -> {
+                capitalizeNext = true
+            }
+            i == 0 -> {
+                append(c.lowercase())
+            }
+            c.isDigit() -> {
+                append(c)
+                capitalizeNext = true
+            }
+            else -> {
+                append(if (capitalizeNext) c.uppercase() else c)
+                capitalizeNext = false
+            }
+        }
     }
+}
 
 // We want to support both `-` and `_`, so...
 private fun String.hyphenToUnderscore() = replace("-", "_")
