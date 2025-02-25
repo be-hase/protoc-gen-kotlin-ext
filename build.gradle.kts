6000eecaeb5801844e29fea2b5184001464b8f07
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2025-02-25 10:09:30 +0900

    fix release workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
-val isOnCI = System.getenv()["GITHUB_ACTIONS"] != null
-
 allprojects {
     group = "dev.hsbrysk"
-    version = "1.0.1" + if (isOnCI) "" else "-SNAPSHOT"
+
+    val defaultVersion = "latest-SNAPSHOT"
+    version = providers.gradleProperty("publishVersion").orNull
+        ?: providers.environmentVariable("PUBLISH_VERSION").orNull
+            ?: defaultVersion
 }
